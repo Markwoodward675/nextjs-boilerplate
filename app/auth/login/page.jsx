8999e7974808d5aad2ee5d6cd93d9b3253d912a5
Markwoodward675 <markwoodward.voiceofuk@gmail.com>
2025-12-14 10:06:16 +0100

    Update page.jsx

--- app/auth/login/page.jsx
+++ app/auth/login/page.jsx
@@ -1,11 +1,5 @@
-"use client";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default function LoginRedirect() {
-  const router = useRouter();
-  useEffect(() => {
-    router.replace("/signin");
-  }, [router]);
-  return null;
+  redirect("/signin");
 }
