599a874dde438de54e8ddd76e469269951e16aa0
Markwoodward675 <markwoodward.voiceofuk@gmail.com>
2025-12-16 16:51:10 +0100

    Create BrandLogo.jsx

---
+++ components/BrandLogo.jsx
@@ -0,0 +1,19 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+export default function BrandLogo({ size = 36 }) {
+  const [src, setSrc] = useState("/logo-mark.png");
+
+  return (
+    <Image
+      src={src}
+      alt="Day Trader"
+      width={size}
+      height={size}
+      priority
+      onError={() => setSrc("/favicon-32x32.png")}
+    />
+  );
+}
