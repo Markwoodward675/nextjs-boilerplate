7554783bfaec9f61862f33d912841af3f7489bf2
Markwoodward675 <markwoodward.voiceofuk@gmail.com>
2025-12-19 21:16:37 +0100

    Update page.jsx

--- app/signout/page.jsx
+++ app/signout/page.jsx
@@ -1,16 +1,18 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { signOut } from "../../lib/api";
 
 export default function SignoutPage() {
   const router = useRouter();
+  const [msg, setMsg] = useState("Signing you out…");
 
   useEffect(() => {
     (async () => {
       try {
         await signOut();
+        setMsg("Signed out. Redirecting…");
       } finally {
         router.replace("/signin");
       }
@@ -18,12 +20,12 @@ export default function SignoutPage() {
   }, [router]);
 
   return (
-    <div className="dt-shell" style={{ paddingTop: 28 }}>
+    <div className="dt-shell" style={{ paddingTop: 26 }}>
       <div className="contentCard">
         <div className="contentInner">
           <div className="card">
-            <div className="cardTitle">Signing you out…</div>
-            <div className="cardSub" style={{ marginTop: 6 }}>Please wait.</div>
+            <div className="cardTitle">Sign out</div>
+            <div className="cardSub" style={{ marginTop: 6 }}>{msg}</div>
           </div>
         </div>
       </div>
